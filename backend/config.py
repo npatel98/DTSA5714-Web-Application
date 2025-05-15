77d40baadbf2211a629d0daac220c74e263e5a67
Nikhil Patel <nikhil.patel@colorado.edu>
2025-05-14 21:47:49 -0700

    Database creation

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLAlchemy_DATABASE_URI"] = "sqlite:///mydatabase.db"
-app.config["SQLAlchemy_TRACK_MODIFICATIONS"] = False
+basedir = os.path.abspath(os.path.dirname(__name__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'database', 'database.db')
+app.config["SQLAlCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, directory=os.path.join(basedir, 'database'))
